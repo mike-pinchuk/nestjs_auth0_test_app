52b45505096c779e0debef902457d762a5348d31
Mykhailo Pinchuk <m.pinchuk@mobidev.biz>
2021-08-22 15:46:48 +0300

    feat(app): app initialization

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ItemsModule } from './items/items.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [ItemsModule, AuthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
