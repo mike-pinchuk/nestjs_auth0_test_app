9d7e7af89147d51aaedfc6ea2d9b4c2744064a67
Mykhailo Pinchuk <m.pinchuk@mobidev.biz>
2021-08-23 17:33:10 +0300

    (auth): changed jwt.strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -25,6 +25,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   validate(payload: unknown): unknown {
+    console.log('PAYLOAD: ', payload);
     return payload;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       disableErrorMessages: true,
